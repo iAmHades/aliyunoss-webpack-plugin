9aecd9a5f765ac066ecb90b90a64bb4415a901e5
name <name@names-Mac-mini.local>
2016-03-09 09:03:39 +0800

    1:调整文档 2:新增单独使用模式

--- README.md
+++ README.md
@@ -22,14 +22,41 @@ var webpackConfig = {
     filename: 'index_bundle.js'
   },
   plugins: [new AliyunossWebpackPlugin({
-    buildPath:__dirname+'/build',
+    buildPath:'your build path',
     region: 'your region',
     accessKeyId: 'your key',
     accessKeySecret: 'your secret',
-    bucket: 'your bucket'
+    bucket: 'your bucket',
+    getObjectHeaders: function(filename) {
+      return {
+        Expires: 6000
+      }
+    }
   })]
 }
-```
+```   
+
+don't depend webpack,just as follows:
+```javascript
+var AliyunossWebpackPlugin = require('aliyunoss-webpack-plugin')
+var oss = new AliyunossWebpackPlugin({
+  buildPath:'your build path',
+  region: 'your region',
+  accessKeyId: 'your key',
+  accessKeySecret: 'your secret',
+  bucket: 'your bucket',
+  deleteAll: true,
+  generateObjectPath: function(filename) {
+    return filename
+  },
+  getObjectHeaders: function(filename) {
+    return {
+      Expires: 6000
+    }
+  }
+});
+oss.apply()
+```     
 
 Configuration
 -------------
@@ -41,5 +68,5 @@ The plugin allowed values are as follows:
 - `accessKeySecret`: 阿里云的权限访问的secret。
 - `bucket`: 阿里云的权限访问的secret。
 - `deleteAll`: 先删除oss上的代码之后再上传，默认为false
-- `generateObjectPath`: 函数，传入上传的文件名，返回一个你想要的oss的文件名，默认是identity，这是给调用者一个机会去修改oss的路径，比如加一个前缀等
-- `getObjectHeaders`: 函数，传入上传的文件名，返回一个对象，里面是headers，具体可以参见ali-oss的文档
+- `generateObjectPath`: 函数（可选），函数参数为上传的文件名，必须返回一个字符串作为文件名，默认为文件原名。通过该函数可以让使用者自定义上传的文件名或者修改oss的路径，比如加一个前缀等
+- `getObjectHeaders`: 函数（可选），函数参数为上传的文件名，返回设置的headers对象，默认为空，具体可以参见[ali-oss的文档](http://doc.oss.aliyuncs.com/#_Toc336676772)
--- index.js
+++ index.js
@@ -28,16 +28,24 @@ function AliyunossWebpackPlugin(options) {
 
 AliyunossWebpackPlugin.prototype.apply = function(compiler) {
 	var _this = this;
-	compiler.plugin("done", function(compilation) {
+	if (compiler) {
+		compiler.plugin("done", function(compilation) {
+			_this.oposs();
+		});
+	} else {
 		_this.oposs();
-	});
+	}
 };
 
 AliyunossWebpackPlugin.prototype.oposs = function() {
 	var _this = this;
-  var deleteAll = _this.options.deleteAll || false;
-  var generateObjectPath = _this.options.generateObjectPath || function(fileName) { return fileName; }
-  var getObjectHeaders = _this.options.getObjectHeaders || function() { return {}; }
+	var deleteAll = _this.options.deleteAll || false;
+	var generateObjectPath = _this.options.generateObjectPath || function(fileName) {
+		return fileName;
+	}
+	var getObjectHeaders = _this.options.getObjectHeaders || function() {
+		return {};
+	}
 
 	co(function*() {
 		'use strict';
@@ -49,18 +57,18 @@ AliyunossWebpackPlugin.prototype.oposs = function() {
 		});
 
 		//删除oss上代码
-    if (deleteAll) {
-      var fileList = yield store.list();
-      var files = [];
-      if (fileList.objects) {
-        fileList.objects.forEach(function(file) {
-          files.push(file.name);
-        })
-        var result = yield store.deleteMulti(files, {
-          quiet: true
-        });
-      }
-    }
+		if (deleteAll) {
+			var fileList = yield store.list();
+			var files = [];
+			if (fileList.objects) {
+				fileList.objects.forEach(function(file) {
+					files.push(file.name);
+				})
+				var result = yield store.deleteMulti(files, {
+					quiet: true
+				});
+			}
+		}
 
 		//上传oss的新代码
 		fileUtils.eachFileSync(_this.options.buildPath, function(filename, stats) {
@@ -70,17 +78,16 @@ AliyunossWebpackPlugin.prototype.oposs = function() {
 		var j = 0;
 		for (var i = 0; i < _this.fileArray.length; i++) {
 			var file = _this.fileArray[i];
-			var fileName = file.replace(_this.options.buildPath, '');
-      if (fileName.startsWith('/')) {
-        fileName = fileName.substr(1);
-      }
-      var ossFileName = generateObjectPath(fileName);
-      if (ossFileName) {
-        yield store.put(ossFileName, file, { headers: getObjectHeaders(fileName) });
-        console.log(file + ' -- upload to ' + ossFileName + ' success'.green);
-      } else {
-        console.log('skipping file ' + file)
-      }
+			var fileName = file.split('/').pop();
+			var ossFileName = generateObjectPath(fileName);
+			if (ossFileName) {
+				yield store.put(ossFileName, file, {
+					headers: getObjectHeaders(fileName)
+				});
+				console.log(file + ' -- upload to ' + ossFileName + ' success'.green);
+			} else {
+				console.log('skipping file ' + file)
+			}
 		}
 	}).catch(function(err) {
 		console.info(err)
